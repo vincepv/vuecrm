a8ccf628ff4fd82e4d98e7986dc3ba7b1553e8fa
vpv <vincentpaulvictor@gmail.com>
2022-06-17 12:09:22 +0200

    store pinia

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// state management
+import { createPinia } from 'pinia'
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
-
+const pinia = createPinia()
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(pinia)
 app.mount('#app')
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,15 @@
+<script setup>
+import {ref} from 'vue';
+import Sold from '@/components/Sold.vue';
+import Condition from '@/components/Condition.vue';
+import {useProductStore} from '@/stores/ProductStore.js';
+
+const store = useProductStore();
+const  title = ref("Default title");
+
+</script>
+
+
 <template>
   <div>
     <h1>About us</h1>
@@ -6,16 +18,14 @@
   </div>
   <Sold @changetitle = "(n)=> title = n" @resettitle="(n)=>title=n"/>
   <Condition />
+
+  <h2>Store pinia test</h2>
+  <p v-for="product in store.products" :key="product.name">
+    Produit : {{product.name}} /
+    Price €: {{product.price}} 
+  </p>
 </template>
 
-<script setup>
-import {ref} from 'vue'
-import Sold from '@/components/Sold.vue'
-import Condition from '@/components/Condition.vue'
-
-const  title = ref("Default title");
-
-</script>
 
 
 
